daf09f336e612307971c50fe2dc5dea9e7f7824d
Josebahuegun <jhuegun25@izarraitz.eus>
2026-09-15 12:25:46 +0200

    Final constant gehitu.

--- src/org/example/kotxea.java
+++ src/org/example/kotxea.java
@@ -4,4 +4,5 @@ public class kotxea extends Ibilgailua{
     public kotxea(String marka, String modeloa, Integer prezioa) {
         super(marka, modeloa, prezioa);
     }
+    final static int GURPIL_KOPURUA = 4;
 }
